359f2ece0bd0831640090068d1822d7ec25229c2
Carlos Herrera <carherco@gmail.com>
2020-02-17 10:43:08 +0100

    Integración de validadores del core de Fonk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from './validators/fonk-validators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fonk-integration';
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      name: new FormControl('Carlos', [Validators.required({trim: false}), Validators.maxLength({length: 20})]),
+      email: new FormControl('carlos@fonk.es', [Validators.required({trim: true}), Validators.email()])
+    });
+
+    console.log(this.userForm.valid);
+    console.log(this.userForm.get('name').errors);
+    console.log(this.userForm.get('email').errors);
+
+    this.userForm.get('name').setValue('Un nombre demasiado largo');
+    console.log(this.userForm.valid);
+    console.log(this.userForm.get('name').errors);
+
+    this.userForm.get('email').setValue('Un email no valido@fonk');
+    console.log(this.userForm.valid);
+    console.log(this.userForm.get('email').errors);
+
+    console.log(this.userForm.errors);
+  }
 }
